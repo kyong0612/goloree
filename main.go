3fcdd1ebec5a3315179eab32210e20156374f06b
kim kiyong <kiyon.kimu@g.smartvalue.ad.jp>
2021-10-27 11:30:30 +0900

    add: spl parser

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pingcap/parser"
+	"github.com/pingcap/parser/ast"
+	_ "github.com/pingcap/parser/test_driver"
+)
 
 func main() {
-	fmt.Println("hello goloree")
+	astNode, err := parse("SELECT a, b FROM t")
+	if err != nil {
+		fmt.Printf("parse error: %v\n", err.Error())
+		return
+	}
+	fmt.Printf("%v\n", *astNode)
+}
+
+func parse(sql string) (*ast.StmtNode, error) {
+	p := parser.New()
+
+	stmtNodes, _, err := p.Parse(sql, "", "")
+	if err != nil {
+		return nil, err
+	}
+	return &stmtNodes[0], nil
 }
